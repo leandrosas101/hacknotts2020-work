b2d84d0fafab76fd3e16dadfd885dd7bd5dd7d9d
Lwa-15 <lwa15@hotmail.co.uk>
2020-11-28 13:07:48 +0000

    Second Javascript Commit

--- Javascript.js
+++ Javascript.js
@@ -1,1 +1,35 @@
-document.getElementsByClassName("body").innerHTML = "Hello World";
+FilterFolders("All")
+FilterRecipes("All")
+function FilterRecipes(RecipeName) {
+    var Recipes = document.getElementsByClassName("Filter");
+    var RecipeIndex;
+    if (RecipeName == "All") {
+        for (RecipeIndex = 0; RecipeIndex < Recipes.length; RecipeIndex++) {
+            Recipes[RecipeIndex].style.display = "block";
+        }
+    }
+    for (RecipeIndex = 0; RecipeIndex < Recipes.length; RecipeIndex++) {
+        if (Recipes[RecipeIndex] != RecipeName) {
+            Recipes[RecipeIndex].style.display = "none";
+        } else {
+            Recipes[RecipeIndex].style.display = "block";
+        }
+    }
+}
+
+function FilterFolders(FolderName) {
+    var Folders = document.getElementsByClassName("Folders")
+    var FolderIndex;
+    if (FolderName == "All") {
+        for (FolderIndex = 0; FolderIndex < Folders.length; FolderIndex++) {
+            Folders[FolderIndex].style.display = "block";
+        }
+    }
+    for (FolderIndex = 0; FolderIndex < Folders.length; FolderIndex++) {
+        if (Folders[FolderIndex] != FolderName) {
+            Folders[FolderIndex].style.display = "none";
+        } else {
+            Folders[FoldersIndex].style.display = "block";
+        }
+    }
+}
