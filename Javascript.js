073530604cdd573c6969808485acf1ef01042aef
Lwa-15 <lwa15@hotmail.co.uk>
2020-11-28 14:29:02 +0000

    Fourth JS Commit

--- Javascript.js
+++ Javascript.js
@@ -1,36 +1,8 @@
-FilterSelection("all")
-FilterRecipes("all")
-
-function FilterSelection(RecipeName) {
-    var Recipes = document.getElementsByClassName("filterDiv");
-    var RecipeIndex;
-    if (RecipeName == "all") {
-        for (RecipeIndex = 0; RecipeIndex < Recipes.length; RecipeIndex++) {
-            Recipes[RecipeIndex].style.display = "block";
-        }
+function openTab(TabName) {
+    var Index;
+    var Tabs = document.getElementsByClassName("containerTab");
+    for (Index = 0; Index < Tabs.length; Index++) {
+        Tabs[Index].style.display = "none";
     }
-    for (RecipeIndex = 0; RecipeIndex < Recipes.length; RecipeIndex++) {
-        if (Recipes[RecipeIndex] != RecipeName) {
-            Recipes[RecipeIndex].style.display = "none";
-        } else {
-            Recipes[RecipeIndex].style.display = "block";
-        }
-    }
-}
-
-function FilterFolders(FolderName) {
-    var Folders = document.getElementsByClassName("Folders")
-    var FolderIndex;
-    if (FolderName == "All") {
-        for (FolderIndex = 0; FolderIndex < Folders.length; FolderIndex++) {
-            Folders[FolderIndex].style.display = "block";
-        }
-    }
-    for (FolderIndex = 0; FolderIndex < Folders.length; FolderIndex++) {
-        if (Folders[FolderIndex] != FolderName) {
-            Folders[FolderIndex].style.display = "none";
-        } else {
-            Folders[FolderIndex].style.display = "block";
-        }
-    }
+    document.getElementById(TabName).style.display = "block";
 }
